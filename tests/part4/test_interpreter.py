5b307c212c9fc94a2896a95d53e46cae8a0aa8ff
Josef Ondřej <josef.ondrej@tipsport.cz>
2023-01-26 21:52:58 +0100

    feat(part 4): Add exercise tests

---
+++ tests/part4/test_interpreter.py
@@ -0,0 +1,18 @@
+import pytest
+
+from pascal_intepreter.part4.interpreter import Lexer, Interpreter
+
+
+@pytest.mark.parametrize("text, expected_result", [
+    ("7", 7),
+    ("3 * 5", 15),
+    ("3 / 2", 1.5),
+    ("2 + 7 * 4", 30),
+    ("7 - 8 / 4", 5),
+    ("14 + 2 * 3 - 6 / 2", 17)
+])
+def test_expr(text, expected_result):
+    lexer = Lexer(text)
+    interpreter = Interpreter(lexer)
+    result = interpreter.expr()
+    assert result == expected_result
