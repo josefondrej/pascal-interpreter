a299262e00544699873e6cbb42d4e2bb6b041117
Josef Ondřej <josef.ondrej@tipsport.cz>
2023-01-26 22:54:26 +0100

    feat(part 5): Implement first exercise

--- pascal_intepreter/part5/exercise_interpreter.py
+++ pascal_intepreter/part5/exercise_interpreter.py
@@ -8,6 +8,10 @@
 # evaluate deeply nested arithmetic expressions like:
 # 7 + 3 * (10 / (12 / (3 + 1) - 1))
 
+INTEGER, DIV, MUL, PLUS, MINUS, EOF = \
+    'INTEGER', 'DIV', 'MUL', 'PLUS', 'MINUS', 'EOF'
+
+
 class Token:
     def __init__(self, type, value):
         self.type = type
@@ -26,8 +30,54 @@ class Lexer:
         self.pos = 0
         self.current_char = self.text[self.pos]
 
+    def error(self):
+        raise Exception('Invalid character')
+
+    def advance(self):
+        self.pos += 1
+        if self.pos > len(self.text) - 1:
+            self.current_char = None
+        else:
+            self.current_char = self.text[self.pos]
+
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def get_integer_token(self):
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return Token(INTEGER, int(result))
+
     def get_next_token(self):
-        pass  # TODO
+        if self.current_char is None:
+            return Token(EOF, None)
+
+        if self.current_char.isspace():
+            self.skip_whitespace()
+
+        if self.current_char.isdigit():
+            return self.get_integer_token()
+
+        if self.current_char == '+':
+            self.advance()
+            return Token(PLUS, '+')
+
+        if self.current_char == '-':
+            self.advance()
+            return Token(MINUS, '-')
+
+        if self.current_char == '*':
+            self.advance()
+            return Token(MUL, '*')
+
+        if self.current_char == '/':
+            self.advance()
+            return Token(DIV, '/')
+
+        self.error()
 
 
 class Interpreter:
@@ -35,5 +85,44 @@ class Interpreter:
         self.lexer = lexer
         self.current_token = self.lexer.get_next_token()
 
+    def error(self):
+        raise Exception('Invalid syntax')
+
+    def eat(self, token_type):
+        if self.current_token.type == token_type:
+            self.current_token = self.lexer.get_next_token()
+        else:
+            self.error()
+
+    def factor(self):
+        token = self.current_token
+        self.eat(INTEGER)
+        return token.value
+
+    def term(self):
+        result = self.factor()
+
+        while self.current_token.type in (MUL, DIV):
+            token = self.current_token
+            if token.type == MUL:
+                self.eat(MUL)
+                result *= self.factor()
+            elif token.type == DIV:
+                self.eat(DIV)
+                result /= self.factor()
+
+        return result
+
     def expr(self):
-        pass  # TODO
+        result = self.term()
+
+        while self.current_token.type in (PLUS, MINUS):
+            token = self.current_token
+            if token.type == PLUS:
+                self.eat(PLUS)
+                result += self.term()
+            elif token.type == MINUS:
+                self.eat(MINUS)
+                result -= self.term()
+
+        return result
