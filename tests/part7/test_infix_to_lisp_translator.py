dfc39397b400ba104194b4f5639085a6205bb6ec
Josef Ondřej <josef.ondrej@tipsport.cz>
2023-02-08 12:20:56 +0100

    feat(part 7): Fix test_infix_to_lisp_translator.py

--- tests/part7/test_infix_to_lisp_translator.py
+++ tests/part7/test_infix_to_lisp_translator.py
@@ -4,9 +4,11 @@ from pascal_intepreter.part7.infix_to_lisp_translator import InfixToLISPTranslat
 
 
 @pytest.mark.parametrize("text, expected_result", [
-    "2 + 3", "(+ 2 3)",
-    "(2 + 3 * 5)", "(+ 2 (* 3 5))"])
+    ("2 + 3", "(+ 2 3)"),
+    ("(2 + 3 * 5)", "(+ 2 (* 3 5))")])
 def test_translate(text, expected_result):
     translator = InfixToLISPTranslator()
     result = translator.translate(text)
+    print(result)
+    print(expected_result)
     assert result == expected_result
