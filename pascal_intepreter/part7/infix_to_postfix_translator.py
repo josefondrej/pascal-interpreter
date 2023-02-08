3ec4198eb4b52dc9dead39a21bb8027bf9019a86
Josef Ondřej <josef.ondrej@tipsport.cz>
2023-02-08 12:24:56 +0100

    feat(part 7): Implement infix_to_postfix_translator.py

--- pascal_intepreter/part7/infix_to_postfix_translator.py
+++ pascal_intepreter/part7/infix_to_postfix_translator.py
@@ -1,6 +1,18 @@
-from pascal_intepreter.part7.spi import NodeVisitor
+from pascal_intepreter.part7.spi import NodeVisitor, Lexer, Parser
+
+OPERATION_TYPE_TO_STRING = {'PLUS': '+', 'MINUS': '-', 'MUL': '*', 'DIV': '/'}
 
 
 class InfixToPostfixTranslator(NodeVisitor):
+    def visit_BinOp(self, node):
+        op_str = OPERATION_TYPE_TO_STRING[node.op.type]
+        return f"{self.visit(node.left)} {self.visit(node.right)} {op_str}"
+
+    def visit_Num(self, node):
+        return node.value
+
     def translate(self, text: str) -> str:
-        raise NotImplementedError
+        lexer = Lexer(text)
+        parser = Parser(lexer)
+        tree = parser.parse()
+        return self.visit(tree)
