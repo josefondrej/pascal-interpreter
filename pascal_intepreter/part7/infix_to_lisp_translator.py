e008dd3ed33ec8b9467269d2fe68b197833e4966
Josef Ondřej <josef.ondrej@tipsport.cz>
2023-02-08 12:21:05 +0100

    feat(part 7): Implement infix_to_lisp_translator.py

--- pascal_intepreter/part7/infix_to_lisp_translator.py
+++ pascal_intepreter/part7/infix_to_lisp_translator.py
@@ -1,6 +1,19 @@
 from pascal_intepreter.part7.interpreter import NodeVisitor
+from pascal_intepreter.part7.spi import Lexer, Parser
+
+OPERATION_TYPE_TO_STRING = {'PLUS': '+', 'MINUS': '-', 'MUL': '*', 'DIV': '/'}
 
 
 class InfixToLISPTranslator(NodeVisitor):
+    def visit_BinOp(self, node):
+        op_str = OPERATION_TYPE_TO_STRING[node.op.type]
+        return f"({op_str} {self.visit(node.left)} {self.visit(node.right)})"
+
+    def visit_Num(self, node):
+        return node.value
+
     def translate(self, text: str) -> str:
-        raise NotImplementedError
+        lexer = Lexer(text)
+        parser = Parser(lexer)
+        tree = parser.parse()
+        return self.visit(tree)
