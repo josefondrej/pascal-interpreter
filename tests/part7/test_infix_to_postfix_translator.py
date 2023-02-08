2c7c68f4dbdd9b9554e2e2b0a11d23af71f8b9b8
Josef Ondřej <josef.ondrej@tipsport.cz>
2023-02-08 12:23:09 +0100

    feat(part 7): Fix test_infix_to_postfix_translator.py

--- tests/part7/test_infix_to_postfix_translator.py
+++ tests/part7/test_infix_to_postfix_translator.py
@@ -4,7 +4,7 @@ from pascal_intepreter.part7.infix_to_postfix_translator import InfixToPostfixTr
 
 
 @pytest.mark.parametrize("text, expected_result", [
-    "(5 + 3) * 12 / 3", "5 3 + 12 * 3 /"])
+    ("(5 + 3) * 12 / 3", "5 3 + 12 * 3 /")])
 def test_translate(text, expected_result):
     translator = InfixToPostfixTranslator()
     result = translator.translate(text)
