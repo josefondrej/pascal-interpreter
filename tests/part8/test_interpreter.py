5dce7b75bdbfe4f4c569ec67b0d7978b1eaace75
Josef Ondřej <josef.ondrej@tipsport.cz>
2023-04-29 17:31:08 +0200

    feat(part 8): Implement exercise

--- tests/part8/test_interpreter.py
+++ tests/part8/test_interpreter.py
@@ -1,9 +1,18 @@
+import pytest
+
 from pascal_intepreter.part8.interpreter import Interpreter
 from pascal_intepreter.part8.lexer import Lexer
 from pascal_intepreter.part8.parser import Parser
 
 
-def test_interpret():
+@pytest.mark.parametrize('text, expected_result', [
+    ('2 + 4 - 5 * (6 + 7) + - - - 9 * 3 - 2 * (((4-3)) * 2)', -90),
+    ('- 3', -3),
+    ('+ 3', 3),
+    ('5 - - - + - 3', 8),
+    ('5 - - - + - (3 + 4) - +2', 10)
+])
+def test_interpret(text, expected_result):
     text = '2 + 4 - 5 * (6 + 7) + - - - 9 * 3 - 2 * (((4-3)) * 2)'
     interpreter = Interpreter(Parser(Lexer(text)))
     assert interpreter.interpret() == -90
